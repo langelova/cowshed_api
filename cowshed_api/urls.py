b043754504e26a8d0583a7b392f753880f61a57f
Lilia Angelova <urb.lili.an@gmail.com>
2024-11-11 17:38:01 +0100

    celery should wait for db

--- cowshed_api/urls.py
+++ cowshed_api/urls.py
@@ -33,4 +33,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("docs/", schema_view.with_ui("swagger", cache_timeout=0)),
+    path("", include("cmanager.urls")),
 ]
